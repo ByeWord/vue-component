cb80bb86b2946740fbac6b6c5a1dad06de81df81
ByeWord <694745028@qq.comm>
2020-01-15 18:14:12 +0800

    init the vue project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
