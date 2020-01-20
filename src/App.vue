42230107acf6ce1d6df3eeecd0798583fac5f559
ByeWord <694745028@qq.comm>
2020-01-20 13:46:54 +0800

    emptye

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,10 @@
 <template>
   <div id="app">
-    <div class="test-row-col"></div>
-    <w-row type="flex">
-        <w-col
-                span="24"
-                :phone="{span:24}"
-                :pad="{span:12}"
-                :narrow-pc="{span:8,offset:16}"
-        >
-            <div class="demo"></div>
-        </w-col>
-        <w-col
-                span="24"
-                :phone="{span:24}"
-                :pad="{span:12}"
-                :narrow-pc="{span:8,offset:16}"
-        ><div class="demo"></div></w-col>
-        <w-col
-                span="24"
-                :phone="{span:24}"
-                :pad="{span:12}"
-                :narrow-pc="{span:8,offset:16}"
-        ><div class="demo"></div></w-col>
-    </w-row>
+
   </div>
 </template>
 
 <script>
-import WRow from '@/components/row.vue';
-import WCol from '@/components/col.vue';
 
 export default {
   name: 'app',
@@ -40,15 +16,5 @@ export default {
 </script>
 
 <style lang="scss">
-  @import "assets/css/normalize.css";
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-  .demo{
-      border: 1px solid crimson;
-      background-color: #CCC;
-      height: 200px;
-  }
+
 </style>
